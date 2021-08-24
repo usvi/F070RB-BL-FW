2c4e0b6dd18c6f92b12b01097562068778672db4
Janne Paalijarvi <jpaalija@gmail.com>
2021-08-24 16:01:29 +0300

    Led blinker runs

--- F070RB_Bootloader/Core/Src/main.c
+++ F070RB_Bootloader/Core/Src/main.c
@@ -63,34 +63,25 @@ static void MX_GPIO_Init(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  uint32_t u32LedCounter = 0;
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+  // Run high frequency for a brief while, then jump
+  for (u32LedCounter = 0; u32LedCounter < 0xA0000; u32LedCounter++)
+  {
+    if ((u32LedCounter % 0x7FFF) == 0)
+    {
+      HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+    }
+  }
+  // Leave LED off
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  // Deinit and jump
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+
+  // But not yet, haha
   while (1)
   {
     /* USER CODE END WHILE */
