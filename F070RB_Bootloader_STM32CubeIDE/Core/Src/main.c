ca8457c9e166f5eb5a30599aef9be83d15f9b5f9
Janne Paalijarvi <jpaalija@gmail.com>
2022-01-15 14:57:09 +0200

    Flawlessly incorporated the flash functions from Cortex-M4 and verified portability.

--- F070RB_Bootloader_STM32CubeIDE/Core/Src/main.c
+++ F070RB_Bootloader_STM32CubeIDE/Core/Src/main.c
@@ -21,6 +21,8 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
+#include "flash_scan_and_jump.h"
+
 #include <string.h>
 
 
@@ -35,26 +37,11 @@ extern uint32_t __flash_fwarea_end;
 
 static void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void vF070rb_DeInitAndJump(uint32_t u32JumpAddress);
 
 
-static void vF070rb_DeInitAndJump(uint32_t u32FwAddress)
+static void vF070rb_DeInit(void)
 {
-  uint32_t u32FirmwareStackPointerAddress = 0;
-  uint32_t u32FirmwareResetHandlerAddress = 0;
-  uint32_t u32FirmwareOffset = u32FwAddress - FLASH_BOOTLOADER_BEGIN;
-  uint32_t* pu32FwFlashPointer = (uint32_t*)u32FwAddress;
-  uint32_t u32RegistersChecksum = 0;
-
-  // Read 4 first bytes from FW, the stack pointer
-  u32FirmwareStackPointerAddress = *pu32FwFlashPointer;
-  // Read 4 next bytes from FW, reset handler address
-  pu32FwFlashPointer++;
-  u32FirmwareResetHandlerAddress = *pu32FwFlashPointer;
-  // Patch it with offset
-  u32FirmwareResetHandlerAddress += u32FirmwareOffset;
-
-  // Deinitialization and jump parts from
+  // Deinitialization parts from
   // https://github.com/viktorvano/STM32-Bootloader/blob/master/STM32F103C8T6_Bootloader/Core/Inc/bootloader.h
 
   __disable_irq();
@@ -69,36 +56,6 @@ static void vF070rb_DeInitAndJump(uint32_t u32FwAddress)
   SysTick->CTRL = 0;
   SysTick->LOAD = 0;
   SysTick->VAL = 0;
-
-  // Firmware does all the rest needed, system memory remapping, vector table and got operations, etc.
-
-  // Calculate simple checksum of the registers to be passed
-  u32RegistersChecksum = u32FwAddress ^ u32FirmwareOffset;
-
-  // Store firmware absolute address to r10 via hoop if we had Cortex-M0
-  // NOTE: INSPECT WITH INSTRUCTION STEPPING MODE THAT r6 IS FREE!
-  asm ("ldr r6, %0; mov r10, r6"
-      :"=m"(u32FwAddress)
-      :
-      :);
-
-  // Store firmware offset to r11 via hoop if we had Cortex-M0
-  // NOTE: INSPECT WITH INSTRUCTION STEPPING MODE THAT r6 IS FREE!
-  asm ("ldr r6, %0; mov r11, r6;"
-      :"=m"(u32FirmwareOffset)
-      :
-      :);
-
-  // Store registers checksum to r12 via hoop if we had Cortex-M0
-  // NOTE: INSPECT WITH INSTRUCTION STEPPING MODE THAT r6 IS FREE!
-  asm ("ldr r6, %0; mov r12, r6;"
-      :"=m"(u32RegistersChecksum)
-      :
-      :);
-
-  // Actual jump
-  asm("mov sp, %0; bx %1;" : : "r"(u32FirmwareStackPointerAddress), "r"(u32FirmwareResetHandlerAddress));
-
 }
 
 
@@ -109,15 +66,7 @@ static void vF070rb_DeInitAndJump(uint32_t u32FwAddress)
 int main(void)
 {
   uint32_t u32LedCounter = 0;
-  uint8_t au8EmptyFlashBuffer[512] = { 0 };
-  uint8_t au8ReadFlashBuffer[512] = { 0 };
-  uint32_t u32MaxBufReads = (FLASH_FWAREA_END_BOUNDARY - FLASH_FWAREA_BEGIN) / 512;
-  uint32_t u32ReadNum = 0;
   uint32_t u32JumpAddress = FLASH_FWAREA_BEGIN; // Default jump address
-  uint32_t* pu32FwFlashReadPointer = (uint32_t*)FLASH_FWAREA_BEGIN;
-  uint8_t u8Continue = 1;
-
-  memset(au8EmptyFlashBuffer, 0xFF, sizeof(au8EmptyFlashBuffer));
 
   HAL_Init();
   SystemClock_Config();
@@ -126,27 +75,9 @@ int main(void)
   // the LED on during our flash scavenging
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 
-  for (u32ReadNum = 0; (u32ReadNum < u32MaxBufReads) && u8Continue; u32ReadNum++)
-  {
-    memcpy(au8ReadFlashBuffer, pu32FwFlashReadPointer, 512);
+  // Go check if we get better idea about start address than the default
+  vScanMainFirmwareFlashAddress(&u32JumpAddress, FLASH_FWAREA_BEGIN, FLASH_FWAREA_END_BOUNDARY);
 
-    if (memcmp(au8ReadFlashBuffer, au8EmptyFlashBuffer, 512) != 0)
-    {
-      // Found something
-      u32JumpAddress = (uint32_t)pu32FwFlashReadPointer;
-      // Need to go trough in 4 byte increments and see what is here
-      // Use the same things
-      for (u32ReadNum = 0; (u32ReadNum < (512 / 4)) && u8Continue; u32ReadNum++)
-      {
-        if (memcmp(au8EmptyFlashBuffer, pu32FwFlashReadPointer + (u32ReadNum * 4), 4) != 0)
-        {
-          u32JumpAddress += (u32ReadNum * 4);
-          u8Continue = 0;
-        }
-      }
-    }
-    pu32FwFlashReadPointer += 512;
-  }
   // Run high frequency for a brief while, then jump
   for (u32LedCounter = 0; u32LedCounter < 0xA0000; u32LedCounter++)
   {
@@ -157,8 +88,9 @@ int main(void)
   }
   // Leave LED off
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
   // Deinit and jump
-  vF070rb_DeInitAndJump(u32JumpAddress);
+  vDeInitAndJumpToMainFirmware(&vF070rb_DeInit, FLASH_BOOTLOADER_BEGIN, u32JumpAddress);
 
 
   while (1)
